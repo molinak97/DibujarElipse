25bf3660b73d0090d63f369b8a39f4d02e655078
Mquesos <molinavillanuevak@gmail.com>
2019-03-18 17:37:18 -0600

    Algoritmo X y Y no funciona

--- Elipse/Form1.Designer.cs
+++ Elipse/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -88,12 +89,25 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "label4";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(544, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 69);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -114,6 +128,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Elipse/Form1.cs
+++ Elipse/Form1.cs
@@ -20,9 +20,8 @@ namespace Elipse
         int Y2 = 0;
         int Xc = 0;
         int Yc = 0;
-        double r = 0;
-        double a = 0;
-        double b = 0;
+        int Rx = 0;
+        int Ry = 0;
         Pen pen = new Pen(Color.Green, 3);//ALN
         Pen pen1 = new Pen(Color.Red, 3);//MDP
         public Form1()
@@ -43,16 +42,16 @@ namespace Elipse
             {
                 X1 = x;
                 Y1 = y;
-                /*MessageBox.Show(X1 + "," + Y1);//mostrando la cordenada*/
             }
             else if (p - 2 == 0)
             {
                 X2 = x;
                 Y2 = y;
-                /*MessageBox.Show(X2 + "," + Y2);//mostrando la cordenada*/
                 CCentro(X1, Y1, X2, Y2);
+                CRadios(X1, Y1, X2, Y2);
+                CPuntos(X1, Y1, X2, Y2);
                 ElipseALN(X1, Y1, X2, Y2);
-                ElipseMDP(X1, Y1, X2, Y2);
+                /*ElipseMDP(X1, Y1, X2, Y2);*/
                 p = 0;
             }
         }
@@ -86,17 +85,73 @@ namespace Elipse
                     Xc = X2;
                     Yc = Y1;
                     panel1.CreateGraphics().DrawEllipse(pen1, Xc, Yc, 5, 5);
-                }              
+                }           
             }
+        }
+        private void CRadios(int X1, int Y1, int X2, int Y2)
+        {
+            if(X1 == Xc)//verificar cual radios sera RadioY
+            {
+                Ry = Yc - Y1;
+                if(X2 < Xc)
+                {
+                    Rx = Xc - X2;
+                }
+                else
+                {
+                    Rx = X2 - Xc;
+                }
+            }
+            else//RadioX
+            {
+                Ry = Yc - Y2;
+                if (X2 < Xc)
+                {
+                    Rx = Xc - X2;
+                }
+                else
+                {
+                    Rx = X2 - Xc;
+                }
+            }
+
+        }
+        private void CPuntos(int X1, int Y1, int X2, int Y2)
+        {
+            int auxX = 0;
+            int auxY = 0;
+            if (Y1 == Yc)
+            {
+                MessageBox.Show("X1-"+ X1 + "Y1-"+ Y1);
+                MessageBox.Show("X2-" + X2 + "Y2-" + Y2);
+                auxX = X2;
+                auxY = Y2;
+                X2 = X1;
+                Y2 = Y1;
+                X1 = auxX;
+                Y1 = auxY;
+                MessageBox.Show("en Y");
+                MessageBox.Show("X1-" + X1 + "Y1-" + Y1);
+                MessageBox.Show("X2-" + X2 + "Y2-" + Y2);
+            }
+            else
+            {
+                //ElipseALN(X1, Y1, X2, Y2);
+            }
+
         }
         private void ElipseALN(int X1, int Y1, int X2, int Y2)
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-
-
-
-
+            double xd=2, yd=2;
+            int x;
+            int y;
+                for (x = 0; x <= Rx; x++)
+                {
+                    yd = Math.Sqrt((((Math.Pow(Rx, 2)) * (Math.Pow(Ry, 2))) - ((Math.Pow(x, 2)) * (Math.Pow(Ry, 2)))) / Math.Pow(Rx, 2));
+                    panel1.CreateGraphics().DrawEllipse(pen, x + Xc, -(int)yd + Yc, 5, 5);
+                }
             label3.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
         }
         private void ElipseMDP(int X1, int Y1, int X2, int Y2)
@@ -107,6 +162,11 @@ namespace Elipse
 
 
             label3.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
         }
     }
 }

cf65941bd6825f3e5388025529b88f0f7cc43d8f
Mquesos <molinavillanuevak@gmail.com>
2019-03-18 12:58:59 -0600

    Obteniendo Centro del elipse

--- Elipse/Form1.Designer.cs
+++ Elipse/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(31, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(466, 383);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(540, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ALN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(649, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MIDPOINT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(541, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(679, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Elipse/Form1.cs
+++ Elipse/Form1.cs
@@ -7,14 +7,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Elipse
 {
     public partial class Form1 : Form
     {
+        int p = 0;//contador puntos
+        int X1 = 0;
+        int Y1 = 0;
+        int X2 = 0;
+        int Y2 = 0;
+        int Xc = 0;
+        int Yc = 0;
+        double r = 0;
+        double a = 0;
+        double b = 0;
+        Pen pen = new Pen(Color.Green, 3);//ALN
+        Pen pen1 = new Pen(Color.Red, 3);//MDP
         public Form1()
         {
             InitializeComponent();
+            label3.Text = "";
+            label4.Text = "";
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            p++;
+            int x, y;
+            x = e.X;
+            y = e.Y;
+            panel1.CreateGraphics().DrawEllipse(pen, x, y, 5, 5);
+            if (p - 1 == 0)
+            {
+                X1 = x;
+                Y1 = y;
+                /*MessageBox.Show(X1 + "," + Y1);//mostrando la cordenada*/
+            }
+            else if (p - 2 == 0)
+            {
+                X2 = x;
+                Y2 = y;
+                /*MessageBox.Show(X2 + "," + Y2);//mostrando la cordenada*/
+                CCentro(X1, Y1, X2, Y2);
+                ElipseALN(X1, Y1, X2, Y2);
+                ElipseMDP(X1, Y1, X2, Y2);
+                p = 0;
+            }
+        }
+        private void CCentro(int X1, int Y1, int X2, int Y2)
+        {
+            if(X1 > X2)
+            {
+                if(Y1 > Y2)
+                {
+                    Xc = X2;
+                    Yc = Y1;
+                    panel1.CreateGraphics().DrawEllipse(pen1,Xc,Yc, 5, 5);
+                }
+                else
+                {
+                    Xc = X1;
+                    Yc = Y2;
+                    panel1.CreateGraphics().DrawEllipse(pen1, Xc, Yc, 5, 5);
+                }
+            }
+            else
+            {
+                if(Y1 < Y2)
+                {
+                    Xc = X1;
+                    Yc = Y2;
+                    panel1.CreateGraphics().DrawEllipse(pen1, Xc, Yc, 5, 5);
+                }
+                else
+                {
+                    Xc = X2;
+                    Yc = Y1;
+                    panel1.CreateGraphics().DrawEllipse(pen1, Xc, Yc, 5, 5);
+                }              
+            }
+        }
+        private void ElipseALN(int X1, int Y1, int X2, int Y2)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+
+
+
+            label3.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
+        }
+        private void ElipseMDP(int X1, int Y1, int X2, int Y2)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+
+
+            label3.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
         }
     }
 }

a7fdfe16bec015128b20125ecdd6c83686bcadfc
Mquesos <molinavillanuevak@gmail.com>
2019-03-20 00:29:36 -0600

    Algoritmo DDA Y ALGORITMO_NORMAL para dibujar elipse COMPLETO

--- Elipse/Form1.cs
+++ Elipse/Form1.cs
@@ -49,7 +49,7 @@ namespace Elipse
                 Y2 = y;
                 CCentro(X1, Y1, X2, Y2);
                 CRadios(X1, Y1, X2, Y2);
-                /*ElipseALN(X1, Y1, X2, Y2);*/
+                ElipseALN(X1, Y1, X2, Y2);
                 ElipseMDP(X1, Y1, X2, Y2);
                 p = 0;
             }
@@ -120,7 +120,6 @@ namespace Elipse
                     }
                 }
             }
-
         }
         private void ElipseALN(int X1, int Y1, int X2, int Y2)
         {
@@ -129,28 +128,24 @@ namespace Elipse
             double xd=2, yd=2;
             int x;
             int y;
-            int yy;
-                //for (x = 0; (x/yd)<1; x++)
-                for (x = 0; x<=Rx ; x++)
+                for (x = 0; x<Rx ; x++)
                 {
                     yd = Math.Sqrt((((Math.Pow(Rx, 2)) * (Math.Pow(Ry, 2))) - ((Math.Pow(x, 2)) * (Math.Pow(Ry, 2)))) / Math.Pow(Rx, 2));
 
-                    panel1.CreateGraphics().DrawEllipse(pen, x + Xc, (int)yd + Yc, 5, 5);//(x,y)           7 Octante
-                    panel1.CreateGraphics().DrawEllipse(pen, -x + Xc, (int)yd + Yc, 5, 5);//(-x,y)         6 Octante
-                    panel1.CreateGraphics().DrawEllipse(pen, -x + Xc, -(int)yd + Yc, 5, 5);//(-x,-y)   3 Octante
-                    panel1.CreateGraphics().DrawEllipse(pen, x + Xc, -(int)yd + Yc, 5, 5);//(x,-y)      2 Octante
+                    panel1.CreateGraphics().DrawEllipse(pen1, x + Xc, (int)yd + Yc, 5, 5);//(x,y)           7 Octante
+                    panel1.CreateGraphics().DrawEllipse(pen1, -x + Xc, (int)yd + Yc, 5, 5);//(-x,y)         6 Octante
+                    panel1.CreateGraphics().DrawEllipse(pen1, -x + Xc, -(int)yd + Yc, 5, 5);//(-x,-y)   3 Octante
+                    panel1.CreateGraphics().DrawEllipse(pen1, x + Xc, -(int)yd + Yc, 5, 5);//(x,-y)      2 Octante
 
                 }
-            //yy = (int)yd;
-            //xd = yy + 2;
-            /*for (y = Yc; y>=Ry; y--)
+            for (y = 0; y <= Ry; y++)
             {
-                xd = Math.Sqrt((((Math.Pow(Rx, 2)) * (Math.Pow(Ry, 2))) - ((Math.Pow(y, 2)) * (Math.Pow(Ry, 2)))) / Math.Pow(Ry, 2));
-                panel1.CreateGraphics().DrawEllipse(pen, (int)yd + Xc, -x + Yc, 5, 5);//(y,-x)       1 Octante
-                panel1.CreateGraphics().DrawEllipse(pen, -y + Xc, (int)xd + Yc, 5, 5);//(-y,x)     5 Octante
-                panel1.CreateGraphics().DrawEllipse(pen, -y + Xc, -(int)xd + Yc, 5, 5);//(-y,-x)   4 Octante  
-                panel1.CreateGraphics().DrawEllipse(pen, y + Xc, (int)xd + Yc, 5, 5);//(y,x)       8 Octante
-            }*/
+                xd = Math.Sqrt((((Math.Pow(Rx, 2)) * (Math.Pow(Ry, 2))) - ((Math.Pow(y, 2)) * (Math.Pow(Rx, 2)))) / Math.Pow(Ry, 2));
+                panel1.CreateGraphics().DrawEllipse(pen1, (int)xd + Xc, y + Yc, 5, 5);//(x,y)           7 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, -(int)xd + Xc, y + Yc, 5, 5);//(-x,y)         6 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, -(int)xd + Xc, -y + Yc, 5, 5);//(-x,-y)   3 Octante
+                panel1.CreateGraphics().DrawEllipse(pen1, (int)xd + Xc, -y + Yc, 5, 5);//(x,-y)      2 Octante
+            }
             label3.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
         }
         private void ElipseMDP(int X1, int Y1, int X2, int Y2)
@@ -159,9 +154,9 @@ namespace Elipse
             sw.Start();
             double p1, p2;
             int x = 0;
-            int y = Rx;
-            double rx2 = Math.Pow(Ry, 2);
-            double ry2 = Math.Pow(Rx, 2);
+            int y = Ry;
+            double rx2 = Math.Pow(Rx, 2);
+            double ry2 = Math.Pow(Ry, 2);
             p1 = ry2 - (rx2 * Ry) + (0.25 * rx2);
             while ((ry2 * x) < (rx2 * y))
             {
@@ -176,10 +171,10 @@ namespace Elipse
                     y--;
                     p1 = p1 + (2 * ry2 * x) - (2 * rx2 * y) + ry2;
                 }
-                panel1.CreateGraphics().DrawEllipse(pen, y + Xc, x + Yc, 5, 5);//(x,y)           7 Octante
-                panel1.CreateGraphics().DrawEllipse(pen, -y + Xc, x + Yc, 5, 5);//(-x,y)         6 Octante
-                panel1.CreateGraphics().DrawEllipse(pen, -y + Xc, -x + Yc, 5, 5);//(-x,-y)   3 Octante
-                panel1.CreateGraphics().DrawEllipse(pen, y + Xc, -x + Yc, 5, 5);//(x,-y)      2 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, x + Xc, y + Yc, 5, 5);//(x,y)           7 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -x + Xc, y + Yc, 5, 5);//(-x,y)         6 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -x + Xc, -y + Yc, 5, 5);//(-x,-y)   3 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, x + Xc, -y + Yc, 5, 5);//(x,-y)      2 Octante
             }
             p2 = (ry2) * Math.Pow((x + 0.5), 2) + (rx2) * Math.Pow((y - 1), 2) - (rx2 * ry2);
             while (y > 0)
@@ -195,14 +190,13 @@ namespace Elipse
                     y--;
                     p2 = p2 + (2 * ry2 * x) - (2 * rx2 * y) + rx2;
                 }
-                panel1.CreateGraphics().DrawEllipse(pen, y + Xc, -x + Yc, 5, 5);//(y,-x)       1 Octante
-                panel1.CreateGraphics().DrawEllipse(pen, -y + Xc, x + Yc, 5, 5);//(-y,x)     5 Octante
-                panel1.CreateGraphics().DrawEllipse(pen, -y + Xc, -x + Yc, 5, 5);//(-y,-x)   4 Octante  
-                panel1.CreateGraphics().DrawEllipse(pen, y + Xc, x + Yc, 5, 5);//(y,x)       8 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, x + Xc, y + Yc, 5, 5);//(x,y)           7 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -x + Xc, y + Yc, 5, 5);//(-x,y)         6 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, -x + Xc, -y + Yc, 5, 5);//(-x,-y)   3 Octante
+                panel1.CreateGraphics().DrawEllipse(pen, x + Xc, -y + Yc, 5, 5);//(x,-y)      2 Octante
             }
-            label3.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
+            label4.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             panel1.Refresh();
